92eeb2074c84b89aa1d898ad6d579934ef92ca8b
RaeesaParker <raeesaparker101@gmail.com>
2022-11-22 11:41:20 +0000

    Set up server.js

---
+++ src/server.js
@@ -0,0 +1,23 @@
+// ---------------------------------- //
+// Including Packages			
+// ---------------------------------- //
+
+// Framework
+const express = require('express');
+const app = express();
+
+// Body Parsing
+app.use(express.json());
+
+// Routing
+const userRouter = require('./users/userRoutes')
+app.use(userRouter);
+
+
+// Set up port
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
