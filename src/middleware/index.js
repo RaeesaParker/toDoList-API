9ce597ceb93f82adf385eaabd52fbfb9851b60ec
RaeesaParker <raeesaparker101@gmail.com>
2022-11-23 09:48:05 +0000

    Add hashing function to middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,3 +1,17 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const User = require('../users/userModels')
+
+// Set Salt Rounds
+let saltRounds = 10;
+
+// Hash passwords
+exports.hashPass = async (request, response, next) => {
+  try {
+      request.body.password = await bcrypt.hash(request.body.password, saltRounds)
+      next()
+  } catch (error) {
+      console.log(error)
+      response.status(500).send({error: error.message})
+  }
+}
